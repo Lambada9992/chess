37b04dfd1db509320b232dc1b0084fbe198e929e
MarcinLaptop <marcin.bobinski9992@gmail.com>
2021-03-07 15:43:04 +0100

    added pawn piece available moves

--- src/model/Board.java
+++ src/model/Board.java
@@ -3,34 +3,41 @@ package model;
 import java.util.ArrayList;
 
 public class Board {
-    public final int boardSize = 8;
+    public static final int boardSize = 8;
+
+    public static final int UP = -boardSize;
+    public static final int DOWN = boardSize;
+    public static final int RIGHT = 1;
+    public static final int LEFT = -1;
+
     private Piece[] board = new Piece[boardSize*boardSize];
+
 
     public void putPiecesOnBoard(){
         //BLACK
-        board[0] = new Piece(0, Piece.Type.ROOK,Piece.Color.BLACK);
-        board[1] = new Piece(1, Piece.Type.KNIGHT,Piece.Color.BLACK);
-        board[2] = new Piece(2, Piece.Type.BISHOP,Piece.Color.BLACK);
-        board[3] = new Piece(3, Piece.Type.QUEEN,Piece.Color.BLACK);
-        board[4] = new Piece(4, Piece.Type.KING,Piece.Color.BLACK);
-        board[5] = new Piece(5, Piece.Type.BISHOP,Piece.Color.BLACK);
-        board[6] = new Piece(6, Piece.Type.KNIGHT,Piece.Color.BLACK);
-        board[7] = new Piece(7, Piece.Type.ROOK,Piece.Color.BLACK);
+        board[0] = new Piece(0, Piece.Type.ROOK,Piece.Color.BLACK,this);
+        board[1] = new Piece(1, Piece.Type.KNIGHT,Piece.Color.BLACK,this);
+        board[2] = new Piece(2, Piece.Type.BISHOP,Piece.Color.BLACK,this);
+        board[3] = new Piece(3, Piece.Type.QUEEN,Piece.Color.BLACK,this);
+        board[4] = new Piece(4, Piece.Type.KING,Piece.Color.BLACK,this);
+        board[5] = new Piece(5, Piece.Type.BISHOP,Piece.Color.BLACK,this);
+        board[6] = new Piece(6, Piece.Type.KNIGHT,Piece.Color.BLACK,this);
+        board[7] = new Piece(7, Piece.Type.ROOK,Piece.Color.BLACK,this);
         for(int i=boardSize ; i<2*boardSize;i++){
-            board[i] = new Piece(i,Piece.Type.PAWN,Piece.Color.BLACK);
+            board[i] = new Piece(i,Piece.Type.PAWN,Piece.Color.BLACK,this);
         }
 
         //WHITE
-        board[(boardSize*(boardSize-1))+0] = new Piece((boardSize*(boardSize-1))+0, Piece.Type.ROOK,Piece.Color.WHITE);
-        board[(boardSize*(boardSize-1))+1] = new Piece((boardSize*(boardSize-1))+1, Piece.Type.KNIGHT,Piece.Color.WHITE);
-        board[(boardSize*(boardSize-1))+2] = new Piece((boardSize*(boardSize-1))+2, Piece.Type.BISHOP,Piece.Color.WHITE);
-        board[(boardSize*(boardSize-1))+3] = new Piece((boardSize*(boardSize-1))+3, Piece.Type.QUEEN,Piece.Color.WHITE);
-        board[(boardSize*(boardSize-1))+4] = new Piece((boardSize*(boardSize-1))+4, Piece.Type.KING,Piece.Color.WHITE);
-        board[(boardSize*(boardSize-1))+5] = new Piece((boardSize*(boardSize-1))+5, Piece.Type.BISHOP,Piece.Color.WHITE);
-        board[(boardSize*(boardSize-1))+6] = new Piece((boardSize*(boardSize-1))+6, Piece.Type.KNIGHT,Piece.Color.WHITE);
-        board[(boardSize*(boardSize-1))+7] = new Piece((boardSize*(boardSize-1))+7, Piece.Type.ROOK,Piece.Color.WHITE);
+        board[(boardSize*(boardSize-1))+0] = new Piece((boardSize*(boardSize-1))+0, Piece.Type.ROOK,Piece.Color.WHITE,this);
+        board[(boardSize*(boardSize-1))+1] = new Piece((boardSize*(boardSize-1))+1, Piece.Type.KNIGHT,Piece.Color.WHITE,this);
+        board[(boardSize*(boardSize-1))+2] = new Piece((boardSize*(boardSize-1))+2, Piece.Type.BISHOP,Piece.Color.WHITE,this);
+        board[(boardSize*(boardSize-1))+3] = new Piece((boardSize*(boardSize-1))+3, Piece.Type.QUEEN,Piece.Color.WHITE,this);
+        board[(boardSize*(boardSize-1))+4] = new Piece((boardSize*(boardSize-1))+4, Piece.Type.KING,Piece.Color.WHITE,this);
+        board[(boardSize*(boardSize-1))+5] = new Piece((boardSize*(boardSize-1))+5, Piece.Type.BISHOP,Piece.Color.WHITE,this);
+        board[(boardSize*(boardSize-1))+6] = new Piece((boardSize*(boardSize-1))+6, Piece.Type.KNIGHT,Piece.Color.WHITE,this);
+        board[(boardSize*(boardSize-1))+7] = new Piece((boardSize*(boardSize-1))+7, Piece.Type.ROOK,Piece.Color.WHITE,this);
         for(int i=boardSize*(boardSize-2); i<boardSize*(boardSize-1);i++){
-            board[i] = new Piece(i,Piece.Type.PAWN,Piece.Color.WHITE);
+            board[i] = new Piece(i,Piece.Type.PAWN,Piece.Color.WHITE,this);
         }
     }
 
@@ -78,6 +85,8 @@ public class Board {
 
     public void movePiece(Piece piece,int toPosition){
 
+        if(!piece.getAvailableMoves().contains(toPosition)) return;
+
         if(board[toPosition]!=null){
             if (board[toPosition].getPieceColor()==piece.getPieceColor()){
                 return;
@@ -87,5 +96,10 @@ public class Board {
         board[piece.getPosition()] = null;
         board[toPosition] = piece;
         piece.setPosition(toPosition);
+        piece.setN_moves(piece.getN_moves()+1);
+    }
+
+    public Piece getPiece(int index){
+        return board[index];
     }
 }
--- src/model/Piece.java
+++ src/model/Piece.java
@@ -12,11 +12,13 @@ public class Piece {
     private Color color;
     private boolean isDead = false;
     private int n_moves = 0;
+    private Board board;
 
-    protected Piece(int position,Type type,Color color){
+    protected Piece(int position,Type type,Color color,Board board){
         this.position = position;
         this.type = type;
         this.color = color;
+        this.board = board;
     }
 
     public Type getPieceType(){
@@ -43,13 +45,89 @@ public class Piece {
         return isDead;
     }
 
-    public HashSet<Integer> getAvailableMoves(Piece piece){
+    public int getN_moves() {
+        return n_moves;
+    }
+
+    public void setN_moves(int n_moves) {
+        this.n_moves = n_moves;
+    }
+
+    public HashSet<Integer> getAvailableMoves(){
         HashSet<Integer> result = new HashSet<>();
-
-
-
-
+            switch (this.type){
+                case PAWN:
+                    getPawnMoves(result,this);
+                    break;
+                case BISHOP:
+                    break;
+                case KNIGHT:
+                    break;
+                case KING:
+                    break;
+                case QUEEN:
+                    break;
+                case ROOK:
+                    break;
+            }
         return result;
     }
+
+    private void getPawnMoves(HashSet<Integer> moves,Piece piece){
+        int movedirection = getPieceColor() == Color.WHITE? Board.UP : Board.DOWN;
+
+        //1 tile + 2 tile Up/Down
+        if(getPosition()+movedirection>=0 && getPosition()+movedirection<Board.boardSize*Board.boardSize){
+            if(board.getPiece(getPosition()+movedirection)==null){
+                moves.add(getPosition()+movedirection);
+                if(n_moves == 0){
+                    if(getPosition()+2*movedirection>=0 ||
+                            getPosition()+2*movedirection<Board.boardSize*Board.boardSize){
+                        if(board.getPiece(getPosition()+2*movedirection)==null){
+                            moves.add(getPosition()+2*movedirection);
+                        }
+                    }
+                }
+            }
+        }
+
+        //kill another piece
+        if(getPosition()+movedirection>=0 && getPosition()+movedirection<Board.boardSize*Board.boardSize){
+            int positionInNextRow = getPosition()+movedirection;
+            //left
+            if(getPosition()%Board.boardSize!=0){
+                if(board.getPiece(positionInNextRow+Board.LEFT)!=null){
+                    if(board.getPiece(positionInNextRow+Board.LEFT).getPieceColor()!=getPieceColor()){
+                        moves.add(positionInNextRow+Board.LEFT);
+                    }
+                }
+            }
+            //right
+            if(Board.boardSize - (getPosition()%Board.boardSize)!=0){
+                if(board.getPiece(positionInNextRow+Board.RIGHT)!=null){
+                    if(board.getPiece(positionInNextRow+Board.RIGHT).getPieceColor()!=getPieceColor()){
+                        moves.add(positionInNextRow+Board.RIGHT);
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void getBishopMoves(HashSet<Integer> moves,Piece piece){
+
+    }
+    private void getKnightMoves(HashSet<Integer> moves,Piece piece){
+
+    }
+    private void getKingMoves(HashSet<Integer> moves,Piece piece){
+
+    }private void getQueenMoves(HashSet<Integer> moves,Piece piece){
+
+    }
+    private void getRookMoves(HashSet<Integer> moves,Piece piece){
+
+    }
+
 
 }
